9822c58dcf94157a5f05dc263a74740e6a9c5533
mj2sdev <mj2sdev@gmail.com>
2023-08-23 16:19:45 +0900

    방명록 리스트 역순정렬

--- js/note.js
+++ js/note.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-const q = query(collection(db, "note"), orderBy("date"));
+const q = query(collection(db, "note"), orderBy("date", "desc"));
 
 const unsubscribe = onSnapshot(q, (querySnapshot) => {
   const notes = [];
